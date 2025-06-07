e5698c9590c00e672091b7f610a768c6e710fe7d
abdelrahman-2513 <ab19622001@gmail.com>
2025-06-07 18:22:38 +0300

    Proxy server adjustment

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
         target: "https://app.wewantwaste.co.uk",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
